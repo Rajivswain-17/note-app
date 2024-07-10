ab9d461f1db059dc5d68440c51fb1f1448a6b04a
Rajivswain-17 <168177472+Rajivswain-17@users.noreply.github.com>
2024-07-10 20:41:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,4 @@
-//  const notesContainer = document.querySelector(".notes-container");
-//  const createBtn = document.querySelector(".btn");
-//  let notes = document.querySelectorAll(".input-box");
 
-//  createBtn.addEventListener("click", ()=> {
-//      let inputbox = document.createElement("p")
-//      let img = document.createElement("img")
-//      inputbox.className = "input-box"
-//      inputbox.setAttribute("contenteditable", "true");
-//    img.src = "delete.png";
-//     notesContainer.appendChild(inputbox).appendChild(img);
-    
-//  })
-
-
-// notesContainer.addEventListener("click", function(e) {
-//    if (e.target.tagName === "IMG") {
-//     e.target.parentElement.remove();
-    
-//    }
-
-//  })
 
  const notescontainer = document.querySelector(".notes-container");
 const btn = document.querySelector(".btn");
